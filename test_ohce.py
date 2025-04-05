b69b03b7e4de55c0c414c13517ba6576230f5e2f
Javier Leiva <jaleiva2020@udec.cl>
2025-04-05 15:03:40 -0300

    test: added tests for time based greetings, in spanish

--- test_ohce.py
+++ test_ohce.py
@@ -4,3 +4,15 @@ def test_reverses_input():
     ohce = Ohce()
     result = ohce.process("hello")
     assert result == "olleh"
+
+def test_greeting_buenos_dias():
+    ohce = Ohce(user="User", hour=8)
+    assert ohce.greet() == "¡Buenos días User!"
+
+def test_greeting_buenas_tardes():
+    ohce = Ohce(user="User", hour=15)
+    assert ohce.greet() == "¡Buenas tardes User!"
+
+def test_greeting_buenas_noches():
+    ohce = Ohce(user="User", hour=22)
+    assert ohce.greet() == "¡Buenas noches User!"
