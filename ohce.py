ceaf0fb7a49664eadba1bd62914b20b653ee0335
Javier Leiva <jaleiva2020@udec.cl>
2025-04-05 15:08:10 -0300

    feat: program now greets user based on current time

--- ohce.py
+++ ohce.py
@@ -1,4 +1,18 @@
+from datetime import datetime
+
 class Ohce:
+    def __init__(self, user="Usuario", hour=None):
+        self.user = user
+        self.hour = hour if hour is not None else datetime.now().hour
+
+    def greet(self):
+        if 6 <= self.hour < 12:
+            return f"¡Buenos días {self.user}!"
+        elif 12 <= self.hour < 20:
+            return f"¡Buenas tardes {self.user}!"
+        else:
+            return f"¡Buenas noches {self.user}!"
+        
     def reverse(self, text):
         return text[::-1]
 
